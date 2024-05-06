f4bb78426c468cb3dc496ffb17f767cffd509ad1
rehman-naeem <rehman.naeem@invozone.dev>
2024-05-06 23:32:07 +0500

    path

--- app/test.py
+++ app/test.py
@@ -8,7 +8,7 @@ chrome_options.add_argument("--headless")  # Run Chrome in headless mode (no GUI
 
 # Initialize Chrome driver with specified binary path
 
-service = Service(os.path.join(os.getcwd(), './scraper/chromedrivers/chromedriver111'))
+service = Service(os.path.join(os.getcwd(), 'scraper/chromedrivers/chromedriver'))
 driver = webdriver.Chrome(options=chrome_options, service=service)
 
 # Example usage: Open Google and get title
